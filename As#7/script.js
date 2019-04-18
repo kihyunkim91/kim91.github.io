537972088b0f885656cf97ac3ec595f36486e2f2
kihyunkim91 <kihyunkim91@gmail.com>
2019-04-17 22:03:22 -0400

    As #7

---
+++ As#7/script.js
@@ -0,0 +1,61 @@
+function squareNumber() {
+   var input = document.getElementById('square-input').value;
+   var resultNumber = Math.pow(input, 2);
+   var result = "The result of squaring the number " + input + " is " + resultNumber  + ".";
+   document.getElementById('solution1').innerHTML = result;
+   
+};
+function HalfNumber() {
+   var input = document.getElementById('half-input').value;
+   var resultNumber = input / 2;
+   var result =  "Half of " + input + " is " + resultNumber  + ".";
+   document.getElementById('solution1').innerHTML = result;
+   
+};
+function percentOf() {
+   var input1 = document.getElementById('percent1-input').value;
+   var input2 = document.getElementById('percent2-input').value;
+   var resultNumber = input1 / input2 * 100;
+   var result =  input1 + " is " + resultNumber.toFixed(2)  + "% of " + input2 + ".";
+   document.getElementById('solution1').innerHTML = result;
+   
+};
+function areaOfCircle() {
+   var input = document.getElementById('area-input').value;
+   var resultNumber = Math.pow(input, 2) * Math.PI;
+   var result = "The result of squaring the number " + input + " is " + resultNumber.toFixed(2)  + ".";
+   document.getElementById('solution1').innerHTML = result;
+   
+};
+
+
+
+function squareNumber() {
+   var input = document.getElementById('square-input').value;
+   var resultNumber = Math.pow(input, 2);
+   var result = "The result of squaring the number " + input + " is " + resultNumber  + ".";
+   document.getElementById('solution1').innerHTML = result;
+   
+};
+function HalfNumber() {
+   var input = document.getElementById('half-input').value;
+   var resultNumber = input / 2;
+   var result =  "Half of " + input + " is " + resultNumber  + ".";
+   document.getElementById('solution1').innerHTML = result;
+   
+};
+function percentOf() {
+   var input1 = document.getElementById('percent1-input').value;
+   var input2 = document.getElementById('percent2-input').value;
+   var resultNumber = input1 / input2 * 100;
+   var result =  input1 + " is " + resultNumber.toFixed(2)  + "% of " + input2 + ".";
+   document.getElementById('solution1').innerHTML = result;
+   
+};
+function areaOfCircle() {
+   var input = document.getElementById('area-input').value;
+   var resultNumber = Math.pow(input, 2) * Math.PI;
+   var result = "The result of squaring the number " + input + " is " + resultNumber.toFixed(2)  + ".";
+   document.getElementById('solution1').innerHTML = result;
+   
+};
